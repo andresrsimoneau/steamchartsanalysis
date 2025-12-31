d340c5496b8dfd856dac217219ba9b36ea704934
Andres Simoneau <andy20268@gmail.com>
2025-12-31 06:41:19 -0500

    Update import.py

--- SQL/import.py
+++ SQL/import.py
@@ -1,5 +1,16 @@
 import sqlite3
+
+#conn is the variable representing the connection with gamesinfo.db
 conn = sqlite3.connect("gamesinfo.db")
-maingames = conn.cursor()
+
+#maingames is the cursor that allows us to edit the .db file
+games = conn.cursor()
+
+#Creating Games Table
+games.execute()
 
 
+
+
+conn.commit()
+conn.close()
