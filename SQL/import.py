58f9473cb316add8d406c3e652d9ca7cc1bbda87
Andres Simoneau <andy20268@gmail.com>
2025-12-31 16:47:22 -0500

    Update import.py

--- SQL/import.py
+++ SQL/import.py
@@ -102,3 +102,45 @@ conn.close()
 
 
 
+# output generates the following:
+#INCUMBENT VICE PRESIDENT RESULTS SINCE 1948
+#VP George H.W Bush 1988 | 51% approval | 426 EC | WON | PV margin 7.7 pp
+#VP Al Gore 2000 | 57% approval | 266 EC | LOST | PV margin 0.5 pp
+#VP Richard Nixon 1960 | 58% approval | 219 EC | LOST | PV margin -0.17 pp
+#VP Hubert Humphrey 1968 | 42% approval | 191 EC | LOST | PV margin -0.7 pp
+#VP Kamala Harris 2024 | 41% approval | 226 EC | LOST | PV margin -1.3 pp
+#
+#INCUMBENT PRESIDENT RESULTS SINCE 1948
+#POTUS Richard Nixon 1972 | 56% approval | 520 EC | WON | PV margin 23.2 pp
+#POTUS Lyndon B. Johnson 1964 | 74% approval | 486 EC | WON | PV margin 22.6 pp
+#POTUS Ronald Reagan 1984 | 58% approval | 525 EC | WON | PV margin 18.2 pp
+#POTUS Dwight D. Eisenhower 1956 | 68% approval | 457 EC | WON | PV margin 10.9 pp
+#POTUS Bill Clinton 1996 | 54% approval | 379 EC | WON | PV margin 8.5 pp
+#POTUS Harry Truman 1948 | 40% approval | 303 EC | WON | PV margin 4.48 pp
+#POTUS Barack Obama 2012 | 51% approval | 332 EC | WON | PV margin 3.9 pp
+#POTUS George W. Bush 2004 | 48% approval | 286 EC | WON | PV margin 2.4 pp
+#POTUS Gerald Ford 1976 | 45% approval | 240 EC | LOST | PV margin -2.1 pp
+#POTUS Donald Trump 2020 | 45% approval | 232 EC | LOST | PV margin -4.5 pp
+#POTUS George H.W Bush 1992 | 34% approval | 168 EC | LOST | PV margin -5.5 pp
+#POTUS Jimmy Carter 1980 | 37% approval | 49 EC | LOST | PV margin -9.7 pp
+
+#INCUMBENT RESULTS WITH AN APPROVAL RATING OF 45% OR BELOW
+#POTUS Harry Truman 1948 | 40% approval | 303 EC | WON | PV margin 4.48 pp
+#VP Hubert Humphrey 1968 | 42% approval | 191 EC | LOST | PV margin -0.7 pp
+#VP Kamala Harris 2024 | 41% approval | 226 EC | LOST | PV margin -1.3 pp
+#POTUS Gerald Ford 1976 | 45% approval | 240 EC | LOST | PV margin -2.1 pp
+#POTUS Donald Trump 2020 | 45% approval | 232 EC | LOST | PV margin -4.5 pp
+#POTUS George H.W Bush 1992 | 34% approval | 168 EC | LOST | PV margin -5.5 pp
+#POTUS Jimmy Carter 1980 | 37% approval | 49 EC | LOST | PV margin -9.7 pp
+
+#INCUMBENT RESULTS WITH AN APPROVAL RATING OVER 50%
+#POTUS Richard Nixon 1972 | 56% approval | 520 EC | WON | PV margin 23.2 pp
+#POTUS Lyndon B. Johnson 1964 | 74% approval | 486 EC | WON | PV margin 22.6 pp
+#POTUS Ronald Reagan 1984 | 58% approval | 525 EC | WON | PV margin 18.2 pp
+#POTUS Dwight D. Eisenhower 1956 | 68% approval | 457 EC | WON | PV margin 10.9 pp
+#POTUS Bill Clinton 1996 | 54% approval | 379 EC | WON | PV margin 8.5 pp
+#VP George H.W Bush 1988 | 51% approval | 426 EC | WON | PV margin 7.7 pp
+#POTUS Barack Obama 2012 | 51% approval | 332 EC | WON | PV margin 3.9 pp
+#VP Al Gore 2000 | 57% approval | 266 EC | LOST | PV margin 0.5 pp
+#VP Richard Nixon 1960 | 58% approval | 219 EC | LOST | PV margin -0.17 pp
+#[Finished in 266ms]
